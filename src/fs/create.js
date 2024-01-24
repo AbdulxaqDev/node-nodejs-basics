a87b0f3886b7a3cac4fc26eda9a5fe670a67ae82
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-24 12:55:44 +0300

    refactor: format code and add missing semi colon

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,13 @@
-import { writeFile } from "fs/promises"
-import { dirname, join} from 'path';
+import { writeFile }     from 'fs/promises';
+import { dirname, join}  from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname    = dirname(fileURLToPath(import.meta.url));
 
-const fileName = "fresh.txt";
-const fileConteent = "I am fresh and young";
-const filePath = join(__dirname, "files", fileName);
-const errorMessage = "FS operation failed";
+const fileName     = 'fresh.txt';
+const fileConteent = 'I am fresh and young';
+const filePath     = join(__dirname, 'files', fileName);
+const errorMessage = 'FS operation failed';
 
 
 
