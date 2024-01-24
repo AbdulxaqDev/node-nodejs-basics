4dc1e9bce0346002cc5f80a18a28f0542f2df048
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-24 18:15:06 +0300

    refactor: format code and make code readable

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,11 +2,11 @@ import { access, constants, cp } from 'node:fs/promises';
 import { dirname, join}  from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname        = dirname(fileURLToPath(import.meta.url));
+const __dirname      = dirname(fileURLToPath(import.meta.url));
 
-const destFolderPath   = join(__dirname, 'files_copy');
-const srcFolderPath = join(__dirname, 'files');
-const errorMessage     = 'FS operation failed';
+const destFolderPath = join(__dirname, 'files_copy');
+const srcFolderPath  = join(__dirname, 'files');
+const errorMessage   = 'FS operation failed';
 
 async function isFolderExist(folderPath){
   try {
@@ -19,23 +19,22 @@ async function isFolderExist(folderPath){
 
 const copy = async (srcFolderPath, destFolderPath, errorMessage) => {
   try {
+
     if(await isFolderExist(srcFolderPath) 
       && !(await isFolderExist(destFolderPath))){
 
-      await cp(srcFolderPath, destFolderPath, {
-        recursive: true,
-        errorOnExist: true,
-        force: false,
-      });
+        await cp(srcFolderPath, destFolderPath, {
+           recursive: true,
+           errorOnExist: true,
+           force: false,
+        });
 
     }else{
-      throw new Error(errorMessage);
+        throw new Error(errorMessage);
     }
 
   } catch (error) { 
-
     throw new Error(errorMessage);
-
   }
 };
 
