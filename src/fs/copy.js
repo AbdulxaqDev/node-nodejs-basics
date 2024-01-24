b36e760dcda11446d094246985b64d5fc54872b4
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-24 18:13:15 +0300

    feat: completed copy files task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,42 @@
-const copy = async () => {
-    // Write your code here 
+import { access, constants, cp } from 'node:fs/promises';
+import { dirname, join}  from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname        = dirname(fileURLToPath(import.meta.url));
+
+const destFolderPath   = join(__dirname, 'files_copy');
+const srcFolderPath = join(__dirname, 'files');
+const errorMessage     = 'FS operation failed';
+
+async function isFolderExist(folderPath){
+  try {
+    await access(folderPath, constants.F_OK)
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+const copy = async (srcFolderPath, destFolderPath, errorMessage) => {
+  try {
+    if(await isFolderExist(srcFolderPath) 
+      && !(await isFolderExist(destFolderPath))){
+
+      await cp(srcFolderPath, destFolderPath, {
+        recursive: true,
+        errorOnExist: true,
+        force: false,
+      });
+
+    }else{
+      throw new Error(errorMessage);
+    }
+
+  } catch (error) { 
+
+    throw new Error(errorMessage);
+
+  }
 };
 
-await copy();
+await copy(srcFolderPath, destFolderPath, errorMessage);
