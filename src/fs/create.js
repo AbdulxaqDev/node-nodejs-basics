0e48a5d8bcc44397574becb075f2da8a76669859
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-24 12:51:07 +0300

    completed fs create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile } from "fs/promises"
+import { dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const fileName = "fresh.txt";
+const fileConteent = "I am fresh and young";
+const filePath = join(__dirname, "files", fileName);
+const errorMessage = "FS operation failed";
+
+
+
+const create = async (fileConteent, filePath, errorMessage) => {
+  try {
+    await writeFile(filePath, fileConteent, {flag: "wx"});
+  } catch (error) {
+   throw new Error(errorMessage);
+  }
 };
 
-await create();
+await create(fileConteent, filePath, errorMessage);
