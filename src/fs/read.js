d5a370d084c5511ea33e23b6a6b36ec3e7b9e582
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-25 17:18:34 +0300

    feat: complete reading file content

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'node:fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const errorMessage = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+    try{
+        const filePath = join(__dirname, 'files', 'fileToRead.txt');
+        const content = await readFile(filePath, { encoding: 'utf8' });
+        console.log(content);
+    } catch(error){
+        throw new Error(errorMessage)
+    }
+
 };
 
 await read();
