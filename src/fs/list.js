1687f744c41168b40107a6720582f0ad0ec76188
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-25 17:02:14 +0300

    feat: complete listing files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from 'node:fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const errorMessage = 'FS operation failed';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = join(__dirname, 'files');
+    try {
+        const files = await readdir(folderPath);
+        console.log(files);
+    } catch (error) {
+        throw new Error(errorMessage)
+    }
 };
 
 await list();
