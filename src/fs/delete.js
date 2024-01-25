4705c601a6bc6cc64c834f432b90b8a967ea463f
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-25 16:28:38 +0300

    feat: complete file delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,38 @@
+import { access, constants, unlink } from 'node:fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const file = filename => ({
+    name: filename,
+    path: join(__dirname, 'files', filename),
+});
+
+const fileToRemove = file('fileToRemove.txt');
+const errorMessage = 'FS operation failed';
+
+async function isFileExist(filePath) {
+    try {
+        await access(filePath, constants.F_OK)
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+
 const remove = async () => {
-    // Write your code here 
+    const isExist = await isFileExist(fileToRemove.path);
+    try {
+        if (isExist) {
+            unlink(fileToRemove.path);
+        } else {
+            throw new Error(errorMessage);
+        }
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await remove();
