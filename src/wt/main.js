5c4ce706a42c8dc77a401139ef4d522c0c750ac6
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-26 18:51:04 +0300

    init: done main login, need code refoctore

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import { cpus } from "os";
+import { __dirname } from "../helper/dirname.js";
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerFilePath = __dirname(import.meta.url, "worker.js");
+
+  if (isMainThread) {
+    const workersNum = cpus().length;
+
+    for (let i = 0; i < workersNum; i++) {
+      const worker = new Worker(workerFilePath, {
+        workerData: {
+          id: i,
+          task: 10 + (i + 1),
+        },
+      });
+      worker.on("message", (message) => {
+        console.log(`Calculated by ${worker.threadId}: ${message}`);
+      });
+    }
+  }
 };
 
 await performCalculations();
