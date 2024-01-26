8ef1e5a0136cb99063e1cb8df0aed195911f6676
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-26 15:39:23 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { createHash } from "crypto";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  const streamToHash = createReadStream(
+    join(__dirname, "files", "fileToCalculateHashFor.txt")
+  );
+
+  streamToHash.on("data", (data) => {
+    hash.update(data);
+  });
+
+  streamToHash.on("end", () => {
+    const hashedStream = hash.digest("hex");
+    console.log(hashedStream);
+  });
 };
 
 await calculateHash();
