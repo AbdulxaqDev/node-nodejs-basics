bd3a8420bade3bc80f992f5231fbdbca7a3963db
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-26 14:23:07 +0300

    feat: complete cli args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+
+  for (let i = 2; i < args.length; i++) {
+    if (args[i].startsWith("--")) {
+      const propName = args[i].slice(2);
+      const propVal = args[i + 1];
+      console.log(`${propName} is ${propVal}`);
+    }
+  }
 };
 
 parseArgs();
