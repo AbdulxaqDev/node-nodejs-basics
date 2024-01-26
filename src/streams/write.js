45c9507a36b6abd7e2172a80c2b548f57d4c95df
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-26 17:13:28 +0300

    feat: complete streams subtask, write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from "fs";
+import { stdin } from "process";
+
+import { __dirname } from "../helper/dirname.js";
+
+console.log("Click 'Ctrl + D' to close input");
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(
+    __dirname(import.meta.url, "files", "fileToWrite.txt"),
+    { encoding: "utf8"}
+  );
+  stdin.pipe(writableStream);
 };
 
 await write();
