cc0dafe90d681f251dccf9cbbd8fc39c9a9feb68
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-26 18:19:38 +0300

    feat: complete zip subtask, decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import duplexZlib from "./utils/duplexZlib.js";
+import { __dirname } from "../helper/dirname.js";
+
+const filePath = (fileName) => __dirname(import.meta.url, "files", fileName);
+
 const decompress = async () => {
-    // Write your code here 
+  const inputFilePath = filePath("archive.gz");
+  const outputFilePath = filePath("fileToCompress.txt");
+
+  duplexZlib(inputFilePath, outputFilePath, "decompress");
 };
 
 await decompress();
