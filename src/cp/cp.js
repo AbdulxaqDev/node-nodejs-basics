4534f0174bbb497288e49441be576d1b3ee12283
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-29 16:44:24 +0300

    fix: pass arguments correctly

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -5,7 +5,7 @@ import { __dirname } from "../helper/dirname.js";
 const spawnChildProcess = async (args) => {
   const childProcess = spawn(
     "node",
-    [__dirname(import.meta.url, "files", "script.js")],
+    [__dirname(import.meta.url, "files", "script.js"), ...args],
     {
       stdio: ["pipe"],
     }
@@ -13,9 +13,9 @@ const spawnChildProcess = async (args) => {
 
   childProcess.stdout.on("data", (data) => console.log(data.toString()));
 
-  childProcess.stdin.write(args.toString());
+  childProcess.stdin.write("Message from main process");
   childProcess.stdin.end();
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess(["This", "message", "from", "main", "process"]);
+spawnChildProcess(["This", "is", "message"]);
