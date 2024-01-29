31dbf97e2acf084b8280eb3785a3cf091c24f174
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-01-29 18:18:16 +0300

    fix: switch to fork, to ilplement IPC, stdio

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,21 +1,14 @@
-import { spawn } from "node:child_process";
+import { fork } from "node:child_process";
 
 import { __dirname } from "../helper/dirname.js";
 
 const spawnChildProcess = async (args) => {
-  const childProcess = spawn(
-    "node",
-    [__dirname(import.meta.url, "files", "script.js"), ...args],
-    {
-      stdio: ["pipe"],
-    }
-  );
+  const childProcess = fork(
+    __dirname(import.meta.url, "files", "script.js"), args, {silent: true});
 
-  childProcess.stdout.on("data", (data) => console.log(data.toString()));
+  process.stdin.pipe(childProcess.stdin)
 
-  childProcess.stdin.write("Message from main process");
-  childProcess.stdin.end();
+  childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
 spawnChildProcess(["These", "are", "arguments"]);
